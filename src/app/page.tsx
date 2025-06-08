ce7fd5c0104aef954c1de1ace676b0741061f542
Noach Valle <noach1977@gmail.com>
2025-06-08 15:14:22 -0700

    This is a working calculator that I have been working on and just now pushed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -27,7 +27,7 @@ export default function Home() {
 
   return (
     <div className="w-lvh h-lvh text-white bg-slate-400 flex items-center justify-center flex-col ">
-      <h1 className="text-4xl p-5">Welcome to a Calculator-App</h1>
+      <h1 className="text-4xl p-5">Welcome to a Calculator-Project</h1>
       <div className="bg-slate-800 p-5 shadow-lg shadow-slate-900/50 ring-3 rounded-xl h-[30rem] w-[25rem] place-content-center">
         <input readOnly className="w-full h-[4rem] text-2xl bg-slate-600 rounded-md mb-10" type="text" placeholder="0" value={input}/>
         <ul className="grid grid-cols-4 gap-10">
